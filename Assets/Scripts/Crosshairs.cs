30c400d4cea04fa2e65bd0eb405a2d2a0eacb713
gksrn86 <43736348+gksrn86@users.noreply.github.com>
2018-10-14 15:42:40 +0900

    Update Crosshairs.cs

--- Assets/Scripts/Crosshairs.cs
+++ Assets/Scripts/Crosshairs.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Crosshairs : MonoBehaviour {
-
+    //조준점을 만들기 위한 스크립트
+    //照準点を作るためのスクリプト
+	
     public LayerMask targetMask;
     public SpriteRenderer dot;
     public Color dotHighlightColour;
